01c156c2b564bd2391e1430e7058085843cd251a
just <tvarijonasjustas@gmail.com>
2019-03-07 10:33:56 +0200

    added material ui

--- frontend/react/src/components/header/Header.jsx
+++ frontend/react/src/components/header/Header.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+
 import logo from '../../../public/logo.jpg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Exit from '@material-ui/icons/exitToApp';
 
 
 const Header = () => {
   return <header className="main-header">
-    <Link to="/">
+    <Link className="float-left" to="/">
     <img className="header-image logo" src={logo} />
     </Link>
+    <Button variant="contained" color="primary" className="float-right button-size-header">
+      Logout
+      <Exit className="float-right exit-icon">send</Exit>
+    </Button>
   </header>;  
 };
 
