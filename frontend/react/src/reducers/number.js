08f2eaf6fa2c4371aa07fc76912ba417e8f2a6b0
just <tvarijonasjustas@gmail.com>
2019-03-07 09:36:37 +0200

    updated packages

--- frontend/react/src/reducers/number.js
+++ frontend/react/src/reducers/number.js
@@ -6,7 +6,7 @@ const number = (state = initialState,action) => {
     switch(action.type){
         case 'INC_NUMB':
         return {
-            count: state.count + 1
+            count: action.number + 1
         }
         default:
             return state;
