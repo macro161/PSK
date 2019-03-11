7ee846341939ec6c40de3f8f550bf0eab57e2aa8
Matas Savickis <savickismatas@gmail.com>
2019-03-11 13:06:56 +0200

    Big commit

--- frontend/react/src/components/header/Header.jsx
+++ frontend/react/src/components/header/Header.jsx
@@ -2,21 +2,26 @@
 import logo from '../../../public/logo.jpg';
 import { Link } from 'react-router-dom';
 import React from 'react';
+import '../../css/bootstrap.css';
 import Button from '@material-ui/core/Button';
 import Exit from '@material-ui/icons/exitToApp';
 
 
 const Header = () => {
-  return <header className="main-header">
-    <Link className="float-left" to="/">
-      <img className="header-image logo" src={logo} />
-    </Link>
-    <Button variant="contained" color="primary" className="float-right button-size-header">
-      Logout
-      <Exit className="float-right exit-icon">send</Exit>
-    </Button>
-    <hr className="grey-line"/>
-  </header>;  
+  return (
+    <div>
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <img src={logo} width="50%" height="50%" class="d-inline-block align-top" alt=""/>
+        </a>
+      <Button variant="contained" color="primary" className="float-right button-size-header">
+        Logout
+        <Exit className="float-right exit-icon">send</Exit>
+      </Button>
+
+</nav>
+    </div>
+  )
 };
 
 export default Header;
