52f0ed237afeba3c6ebd3a95099df5f6d96de95a
Matas Savickis <savickismatas@gmail.com>
2019-03-18 22:44:15 +0200

    Stiliaus pataisymai

--- frontend/react/src/components/LogInPage/LogIn.jsx
+++ frontend/react/src/components/LogInPage/LogIn.jsx
@@ -17,7 +17,7 @@ class LogIn extends React.Component {
                 <div className="form-container center">
                 <h2>Log In</h2>
                 <div>
-                <svg className="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
+                <svg className="logonew" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
 
                 <TextField
                     id="email"
@@ -28,7 +28,7 @@ class LogIn extends React.Component {
                 />
                 </div>
                 <div>
-                <svg className="logo" xmlns="http://www.w3.org/2000/svg"viewBox="0 0 24 24"><path d="M12.65 10C11.83 7.67 9.61 6 7 6c-3.31 0-6 2.69-6 6s2.69 6 6 6c2.61 0 4.83-1.67 5.65-4H17v4h4v-4h2v-4H12.65zM7 14c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>
+                <svg className="logonew" xmlns="http://www.w3.org/2000/svg"viewBox="0 0 24 24"><path d="M12.65 10C11.83 7.67 9.61 6 7 6c-3.31 0-6 2.69-6 6s2.69 6 6 6c2.61 0 4.83-1.67 5.65-4H17v4h4v-4h2v-4H12.65zM7 14c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>
                 <TextField
                     id="password"
                     label="password"
