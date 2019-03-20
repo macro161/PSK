b84b71a7f27d489eed6ca70baa28c75a9caa5cd3
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2019-03-20 19:42:39 +0200

    initial backend

---
+++ backend/src/test/java/lt/vu/menuliukai/psk/PskApplicationTests.java
@@ -0,0 +1,16 @@
+package lt.vu.menuliukai.psk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PskApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
