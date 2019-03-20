d44828fb26ad43efe81a21b2cbe1702207297445
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2019-03-20 22:15:19 +0200

    add dao

--- backend/src/main/java/lt/vu/menuliukai/psk/controllers/OfficeController.java
+++ backend/src/main/java/lt/vu/menuliukai/psk/controllers/OfficeController.java
@@ -1,43 +1,39 @@
 package lt.vu.menuliukai.psk.controllers;
 
+import lt.vu.menuliukai.psk.dao.OfficeDao;
 import lt.vu.menuliukai.psk.entities.Office;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 @RestController
 @RequestMapping("/office")
 public class OfficeController {
-    private List<Office> offices = new ArrayList<>();
+    @Autowired
+    private OfficeDao officeDao;
 
     private static long id = 102;
 
-    public OfficeController() {
-        offices.add(new Office(100, "Vilnius", "135 Zalgirio g., Vilnius, LT-08217, Lithuania"));
-        offices.add(new Office(101, "Kaunas", "11d. Juozapaviciaus pr., Kaunas, LT-45252, Lithuania"));
-    }
-
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Office> index() {
-        return offices;
+    public Iterable<Office> index() {
+        return officeDao.findAll();
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Office add(@RequestBody Office office) {
-        office.setId(id++);
-        offices.add(office);
-        return office;
+        return officeDao.save(office);
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable long id) {
-        if (!offices.removeIf(office -> office.getId() == id)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "office with id does not exist");
+        try {
+            officeDao.deleteById(id);
+        } catch (EmptyResultDataAccessException exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("office with id %d not found", id));
         }
     }
 }
--- backend/src/main/java/lt/vu/menuliukai/psk/entities/Office.java
+++ backend/src/main/java/lt/vu/menuliukai/psk/entities/Office.java
@@ -1,13 +1,25 @@
 package lt.vu.menuliukai.psk.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @AllArgsConstructor
+@NoArgsConstructor
+@Table(name="OFFICE")
 public class Office {
     @Getter
     @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
 
     @Getter
