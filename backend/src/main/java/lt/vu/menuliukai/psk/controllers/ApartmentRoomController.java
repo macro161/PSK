41c8d2eeab06857de94c66b82c2ad53d6dfb5d5a
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2019-03-31 16:35:58 +0300

    remove apartmentroomconverter

--- backend/src/main/java/lt/vu/menuliukai/psk/controllers/ApartmentRoomController.java
+++ backend/src/main/java/lt/vu/menuliukai/psk/controllers/ApartmentRoomController.java
@@ -1,6 +1,5 @@
 package lt.vu.menuliukai.psk.controllers;
 
-import lt.vu.menuliukai.psk.converters.ApartmentRoomConverter;
 import lt.vu.menuliukai.psk.converters.Converter;
 import lt.vu.menuliukai.psk.dao.ApartmentRoomDao;
 import lt.vu.menuliukai.psk.entities.ApartmentRoom;
@@ -18,21 +17,14 @@ public class ApartmentRoomController {
     @Autowired
     private ApartmentRoomDao apartmentRoomDao;
 
-    @Autowired
-    ApartmentRoomConverter converter;
-
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<ApartmentRoom> index() {
         return apartmentRoomDao.findAll();
     }
 
-    private ApartmentRoom findbyId(long id) {
-        return converter.convert(id);
-    }
-
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ApartmentRoom get(@PathVariable long id) {
-        return findbyId(id);
+        return Converter.convert(apartmentRoomDao, objectName, id);
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
