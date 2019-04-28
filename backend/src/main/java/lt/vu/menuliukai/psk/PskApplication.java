4329969dc1129075e49563a4ec08c705bd8bee00
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2019-04-28 15:50:57 +0300

    remove cruft

--- backend/src/main/java/lt/vu/menuliukai/psk/PskApplication.java
+++ backend/src/main/java/lt/vu/menuliukai/psk/PskApplication.java
@@ -1,12 +1,9 @@
 package lt.vu.menuliukai.psk;
 
-import lt.vu.menuliukai.psk.entities.Trip;
-import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MappedTypes(Trip.class)
 @MapperScan("lt.vu.menuliukai.psk.mappers")
 @SpringBootApplication
 public class PskApplication {
