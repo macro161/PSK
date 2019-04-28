ac52db6a9ca216da1e769647990cd4a3abea0bb2
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2019-04-28 14:19:35 +0300

    use correct path

--- backend/src/main/java/lt/vu/menuliukai/psk/PskApplication.java
+++ backend/src/main/java/lt/vu/menuliukai/psk/PskApplication.java
@@ -2,17 +2,12 @@ package lt.vu.menuliukai.psk;
 
 import lt.vu.menuliukai.psk.entities.Trip;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.type.MappedTypes;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
 
 @MappedTypes(Trip.class)
 @MapperScan("lt.vu.menuliukai.psk.mappers")
@@ -24,9 +19,9 @@ public class PskApplication {
     }
 
     @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
+    public SqlSessionFactory sqlSessionFactory() {
         return new SqlSessionFactoryBuilder().build(
-                ClassLoader.class.getResourceAsStream("MyBatisConfig.xml")
+                ClassLoader.class.getResourceAsStream("mybatis/MyBatisConfig.xml")
         );
     }
 }
