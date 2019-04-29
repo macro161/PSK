17c86badeab79a8a20f85200af3a91507ad7c976
just <31662055+justas68@users.noreply.github.com>
2019-04-29 22:40:22 +0300

    spinner (#47)
    there is a problem, that while spinner is active you can still press buttons. this solves that problem

--- frontend/react/src/components/Spinner.jsx
+++ frontend/react/src/components/Spinner.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
 
 class Spinner extends React.Component {
   render() {
     return (this.props.loading ?
-      <div className='popup'>
+      <Dialog
+      disableBackdropClick = {true}
+      disableEscapeKeyDown
+        open={true}
+        fullWidth={true}
+        fullScreen={true}>
+    <DialogContent>
         <div className='spinner-container'>
           <div className="spinner">
             <div className="circle-outter">
@@ -16,7 +24,8 @@ class Spinner extends React.Component {
             <span>Loading...</span>
           </div>
         </div>
-      </div> : null
+        </DialogContent>
+      </Dialog> : null
     );
   }
 
