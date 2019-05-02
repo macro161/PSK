ab195457034abc3bea97ca8ec63e0ed4403573a0
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2019-05-02 13:31:36 +0300

    Login (#49)
    * implement login mechanism
    
    * remove unused code
    
    * add user endpoint
    
    * make email
    
    * order

---
+++ backend/src/main/java/lt/vu/menuliukai/psk/controllers/RestHomeController.java
@@ -0,0 +1,35 @@
+package lt.vu.menuliukai.psk.controllers;
+
+import lt.vu.menuliukai.psk.dao.EmployeeDao;
+import lt.vu.menuliukai.psk.entities.Employee;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RestHomeController {
+
+    private final EmployeeDao employeDao;
+
+    public RestHomeController(EmployeeDao employeDao) {
+        this.employeDao = employeDao;
+    }
+
+    @RequestMapping("/user")
+    public Employee user() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        Employee employee = employeDao.findByEmail(username);
+        employee.setPassword(null);
+
+        return employee;
+    }
+}
