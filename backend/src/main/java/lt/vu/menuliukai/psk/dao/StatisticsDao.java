57e54708ab79f0662d9e8ef3816004447ede03f4
Džiugas Mažulis <dziugas9950@gmail.com>
2019-05-11 19:54:39 +0300

    The long night (#52)
    * The long night
    
    * Update
    
    * Getting close
    
    * Another one
    
    todo:
    *update single property rather than whole object in reducers
    *change date format from us to gb in sql query
    *fix failing mapper interface injection by constructor in dao and update methods
    
    * Mapper injection and reducer update
    
    * It works
    
    * The dream of spring
    
    *mapper interface implementation
    *query return types changed to return only necessary data
    *extracted statistics interface to implement strategy pattern in other task
    *refactored code
    
    * Changed url to work after resolving confict

--- backend/src/main/java/lt/vu/menuliukai/psk/dao/StatisticsDao.java
+++ backend/src/main/java/lt/vu/menuliukai/psk/dao/StatisticsDao.java
@@ -1,18 +1,54 @@
 package lt.vu.menuliukai.psk.dao;
 
 import lt.vu.menuliukai.psk.entities.Statistics;
+import lt.vu.menuliukai.psk.mappers.StatisticsMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Component
-public class StatisticsDao {
+@RequestScope
+public class StatisticsDao implements StatisticsInterface {
+
     private final SqlSession sqlSession;
+
+    private final StatisticsMapper statisticsMapper;
+
+    private Statistics statistics = new Statistics();
 
     public StatisticsDao(SqlSession sqlSession) {
         this.sqlSession = sqlSession;
+        statisticsMapper = this.sqlSession.getMapper(StatisticsMapper.class);
     }
 
     public Statistics calculateStatistics() {
-        return this.sqlSession.selectOne("calculateStatistics");
+
+        statistics.setMostCommonTripDestination(statisticsMapper.selectMostCommonDestination());
+
+        Statistics temp = statisticsMapper.selectCheapestTrip();
+        statistics.setCheapestTripOrigin(temp.getCheapestTripOrigin());
+        statistics.setCheapestTripDestination(temp.getCheapestTripDestination());
+
+        temp = statisticsMapper.selectMostExpensiveTrip();
+        statistics.setMostExpensiveTripDestination(temp.getMostExpensiveTripDestination());
+        statistics.setMostExpensiveTripOrigin(temp.getMostExpensiveTripOrigin());
+
+        temp = statisticsMapper.selectShortestTrip();
+        statistics.setShortestTripOrigin(temp.getShortestTripOrigin());
+        statistics.setShortestTripDestination(temp.getShortestTripDestination());
+
+        temp = statisticsMapper.selectLongestTrip();
+        statistics.setLongestTripDestination(temp.getLongestTripDestination());
+        statistics.setLongestTripOrigin(temp.getLongestTripOrigin());
+
+        return statistics;
+    }
+
+    public long getEmployeeTripQuantity(String fullName) {
+        return statisticsMapper.selectEmployeeTripQuantity(fullName.replace('_', ' '));
+    }
+
+    public long getPeriodTripQuantity(String leavingDate, String returningDate) {
+        return statisticsMapper.selectPeriodTripQuantity(leavingDate, returningDate);
     }
 }
--- backend/src/main/java/lt/vu/menuliukai/psk/mappers/StatisticsMapper.java
+++ backend/src/main/java/lt/vu/menuliukai/psk/mappers/StatisticsMapper.java
@@ -1,9 +1,17 @@
 package lt.vu.menuliukai.psk.mappers;
 
 import lt.vu.menuliukai.psk.entities.Statistics;
+import org.apache.ibatis.annotations.Param;
 import org.mybatis.cdi.Mapper;
 
 @Mapper
 public interface StatisticsMapper {
-    Statistics calculateStatistics();
+
+    String selectMostCommonDestination();
+    Statistics selectShortestTrip();
+    Statistics selectLongestTrip();
+    Statistics selectMostExpensiveTrip();
+    Statistics selectCheapestTrip();
+    Long selectEmployeeTripQuantity(String fullName);
+    Long selectPeriodTripQuantity(@Param("leavingDate") String leavingDate, @Param("returningDate") String returningDate);
 }
