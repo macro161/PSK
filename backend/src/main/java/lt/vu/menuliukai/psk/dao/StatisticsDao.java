f0bfbd9361a1a333b82947ff5ba8c53f90bd3308
Džiugas Mažulis <dziugas.mazulis@gmail.com>
2019-05-16 16:38:27 +0300

    strategy pattern for statistics
    *create jpa for statistics
    *create app config
    *impement strategy pattern
    *get dao implementation from config

--- backend/src/main/java/lt/vu/menuliukai/psk/dao/StatisticsDao.java
+++ backend/src/main/java/lt/vu/menuliukai/psk/dao/StatisticsDao.java
@@ -1,54 +1,12 @@
 package lt.vu.menuliukai.psk.dao;
 
 import lt.vu.menuliukai.psk.entities.Statistics;
-import lt.vu.menuliukai.psk.mappers.StatisticsMapper;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.RequestScope;
 
-@Component
-@RequestScope
-public class StatisticsDao implements StatisticsInterface {
+public interface StatisticsDao {
 
-    private final SqlSession sqlSession;
+    Statistics calculateStatistics();
 
-    private final StatisticsMapper statisticsMapper;
+    long getEmployeeTripQuantity(String fullName);
 
-    private Statistics statistics = new Statistics();
-
-    public StatisticsDao(SqlSession sqlSession) {
-        this.sqlSession = sqlSession;
-        statisticsMapper = this.sqlSession.getMapper(StatisticsMapper.class);
-    }
-
-    public Statistics calculateStatistics() {
-
-        statistics.setMostCommonTripDestination(statisticsMapper.selectMostCommonDestination());
-
-        Statistics temp = statisticsMapper.selectCheapestTrip();
-        statistics.setCheapestTripOrigin(temp.getCheapestTripOrigin());
-        statistics.setCheapestTripDestination(temp.getCheapestTripDestination());
-
-        temp = statisticsMapper.selectMostExpensiveTrip();
-        statistics.setMostExpensiveTripDestination(temp.getMostExpensiveTripDestination());
-        statistics.setMostExpensiveTripOrigin(temp.getMostExpensiveTripOrigin());
-
-        temp = statisticsMapper.selectShortestTrip();
-        statistics.setShortestTripOrigin(temp.getShortestTripOrigin());
-        statistics.setShortestTripDestination(temp.getShortestTripDestination());
-
-        temp = statisticsMapper.selectLongestTrip();
-        statistics.setLongestTripDestination(temp.getLongestTripDestination());
-        statistics.setLongestTripOrigin(temp.getLongestTripOrigin());
-
-        return statistics;
-    }
-
-    public long getEmployeeTripQuantity(String fullName) {
-        return statisticsMapper.selectEmployeeTripQuantity(fullName.replace('_', ' '));
-    }
-
-    public long getPeriodTripQuantity(String leavingDate, String returningDate) {
-        return statisticsMapper.selectPeriodTripQuantity(leavingDate, returningDate);
-    }
+    long getPeriodTripQuantity(String leavingDate, String returningDate);
 }
