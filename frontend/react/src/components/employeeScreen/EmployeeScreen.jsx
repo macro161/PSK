9f6b8a4792e2521e6232ef71fe4ec401dd26228c
Greta <greta.pyrantaite@gmail.com>
2019-05-23 22:19:29 +0300

    removed console

--- frontend/react/src/components/employeeScreen/EmployeeScreen.jsx
+++ frontend/react/src/components/employeeScreen/EmployeeScreen.jsx
@@ -20,7 +20,6 @@ class EmployeeScreen extends React.Component {
   }
   
   render() {
-    console.log(this.props.travels)
     return (
       <div className='page-frame'>
         <title>Travels</title>
