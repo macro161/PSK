9532cd263956aa2fecbcec3c6e6c5c811e001279
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2019-05-26 22:26:51 +0300

    move employee to service

---
+++ backend/src/main/java/lt/vu/menuliukai/psk/service/EmployeeServiceImpl.java
@@ -0,0 +1,73 @@
+package lt.vu.menuliukai.psk.service;
+
+import lt.vu.menuliukai.psk.PskApplication;
+import lt.vu.menuliukai.psk.converters.Converter;
+import lt.vu.menuliukai.psk.dao.EmployeeDao;
+import lt.vu.menuliukai.psk.dao.OfficeDao;
+import lt.vu.menuliukai.psk.entities.Employee;
+import lt.vu.menuliukai.psk.entities.Office;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private final String objectName = "employee";
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Autowired
+    private OfficeDao officeDao;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Iterable<Employee> findAll() {
+        return employeeDao.findAll();
+    }
+
+    @Override
+    public Employee findById(long id) {
+        return Converter.convert(employeeDao, objectName, id);
+    }
+
+    private String encryptPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    public Employee add(Employee employee) {
+        String role = PskApplication.getUserRole();
+        if (role == null || !role.equals("ADMIN")) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "user needs to have ADMIN role to create users");
+        }
+
+        if (employee.getOffice() == null) {
+            employee.setOffice(officeDao.save(new Office()));
+        }
+
+        if (employee.getRole() == null) {
+            employee.setRole("USER");
+        }
+
+        if (employee.getPassword() != null) {
+            employee.setPassword(encryptPassword(employee.getPassword()));
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password can not be empty");
+        }
+
+        return employeeDao.save(employee);
+    }
+
+    public void delete(long id) {
+        try {
+            employeeDao.deleteById(id);
+        } catch (EmptyResultDataAccessException exception) {
+            Converter.throwException(objectName, id);
+        }
+    }
+}
