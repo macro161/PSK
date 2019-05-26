2a824e0ad6b8c44a7e0328207341a224690d5a1b
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2019-05-26 19:16:25 +0300

    try aspectj

--- backend/src/main/java/lt/vu/menuliukai/psk/AppConfig.java
+++ backend/src/main/java/lt/vu/menuliukai/psk/AppConfig.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class AppConfig {
 
     @Autowired
